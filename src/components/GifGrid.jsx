bf7e9cf5fc6fc78ff5aa4ba0bb1890d79c8f8706
tobias belfiore <tobiasbelfiore@gmail.com>
2022-12-24 21:16:59 -0300

    readme Agregado

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -7,9 +7,7 @@ import { useFetchGifs } from '../hooks/useFetchGifs';
 export const GifGrid = ({category}) => {
 
   const { images, isLoading } = useFetchGifs( category );
-
-
-
+  
   return (
     <>
         <h3>{category}</h3>
